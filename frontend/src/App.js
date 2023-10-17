d78d2e19ac2f13aa0617f63842388618cac6db52
Adarsh Rajan <74054886+adarshrajan11@users.noreply.github.com>
2023-10-17 11:27:43 +0530

    started tge app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css'
+import NavBar from './Components/Navbar/NavBar'
+
+function App() {
+  return (
+    <>
+      <div>
+        <NavBar />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Navbar/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './navbar.css'
+const NavBar = () => {
+  return <div className='navbar'></div>
+}
+
+export default NavBar
